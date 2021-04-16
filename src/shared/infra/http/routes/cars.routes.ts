5b6e592aae5ac5f289973fee7f2f65b9586931d7
Laercio Silva <laercio.sl@hotmail.com>
2021-04-16 18:36:52 -0300

    fix: refactor route post  /specifications

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-import { CreateSpecificationController } from "@modules/cars/useCases/creteSpecification/CreateSpecificationController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
@@ -11,7 +11,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
-const createCarSpecificationController = new CreateSpecificationController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   "/",
