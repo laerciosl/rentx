60b2f987ab95f084b173f9b014d5eed998b972d1
Laercio Silva <laercio.sl@hotmail.com>
2021-04-20 14:06:11 -0300

    fix: refactor docker-compose.yml

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -5,5 +5,8 @@ import { app } from "@shared/infra/http/app";
 describe("Create Category Controller", async () => {
   it("test", async () => {
     await request(app).get("/cars/available").expect(200);
+    return console.log(test);
+
+    // console.log(request(app));
   });
 });
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,8 +1,7 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-
   return createConnection(
     Object.assign(defaultOptions, {
       host,
